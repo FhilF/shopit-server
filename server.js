e7f431bdeb60060e4a162e8bcbda39a36608177f
FhilF <52390799+FhilF@users.noreply.github.com>
2023-03-02 14:44:23 +0800

    my changes

--- server.js
+++ server.js
@@ -25,9 +25,6 @@ db.connect();
 
 if (isProduction) {
   app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
 } else {
   app.use(express.static(path.join(__dirname, "public")));
 }
